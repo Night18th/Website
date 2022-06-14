f8999e088fb5a53946999969146759864691ec04
Gunho Park <pgh0418all@gmail.com>
2022-06-13 20:18:19 -0400

    fist commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import About from './About';
+import Calendar from './Calendar';
 import Home from './Home';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
@@ -10,6 +11,7 @@ import Getintouch from './Getintouch';
 import DarkMode from './DarkMode';
 import Post from './Post';
 import ScrollToTop from './ScrollToTop';
+import Calendarnew from './Calendarnew';
 
 
 export default class Navb extends Component {
@@ -20,14 +22,16 @@ export default class Navb extends Component {
               
       <Navbar collapseOnSelect expand="lg" fixed="top" id="nav">
         <Container>
-          <Navbar.Brand><div id="logo">Your Name Here</div></Navbar.Brand>
+          <Navbar.Brand><div id="logo">Step Up EDR</div></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" id="navbartoggle" />
           <Navbar.Collapse className="basic-navbar-nav" id="nogrow">
             <Nav className="me-auto">
               <Nav.Link eventKey="1" as={Link} to={"/"} id="navlinkoverridetextcolor" className="underline2">Home</Nav.Link>
               <Nav.Link eventKey="2" as={Link} to={"/about"} id="navlinkoverridetextcolor" className="underline2">About</Nav.Link>
               <Nav.Link eventKey="3" as={Link} to={"/blog"} id="navlinkoverridetextcolor" className="underline2">Blog</Nav.Link>
+              <Nav.Link eventKey="4" as={Link} to={"/calendar"} id="navlinkoverridetextcolor" className="underline2">Calendar</Nav.Link>
               <Nav.Link href="/getintouch"><Button id="button"><span id="bold">Get in touch</span></Button></Nav.Link>
+              <Nav.Link href="/calendarnew"><Button id="button"><span id="bold">Calendar</span></Button></Nav.Link>
               <DarkMode />
             </Nav>
           </Navbar.Collapse>
@@ -41,6 +45,8 @@ export default class Navb extends Component {
         <Route path="/blog" exact component={Blog} />
         <Route path='/blog/post/:slug' exact component={Post} />
         <Route path="/getintouch" exact component={Getintouch} /> 
+        <Route path="/calendar" exact component={Calendar} /> 
+        <Route path="/calendarnew" exact component={Calendarnew} /> 
       </Switch>
       </div>
       </Router>
